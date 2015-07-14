74a25ef7d29bb8a3d16e015dd3f9ead346f4bba6
Joseph Jordan <jcjordn5@gmail.com>
2015-07-14 19:12:41 -0400

    httparty gem installed plus bcrypt works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,35 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @users }
+    end
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login!(@user)
+      redirect_to @user
+    else
+      redirect_to new_user_path
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @user }
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 end
