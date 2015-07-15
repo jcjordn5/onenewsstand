967fcb76772d51dfd507f6eaaa4797241c4a2787
Joseph Jordan <jcjordn5@gmail.com>
2015-07-15 13:47:48 -0400

    4 APIs implemented

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,8 @@ class WelcomeController < ApplicationController
   def index
   @guardian = HTTParty.get('http://content.guardianapis.com/search?q=politics&api-key=dc2tmawrmyg2xesbwr6q68d4').to_h["response"]["results"]
   @nytimes = HTTParty.get('http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/1.json?api-key=30ffb766789e445b73dffdb5f1e23483:6:72504693').to_h["results"]
-  puts @guardian
+   @usatoday = HTTParty.get('http://api.usatoday.com/open/articles/topnews/home?count=10&days=1&page=1&encoding=json&api_key=w8g6c8xmqhysh47tejek3yce')
+   @npr = HTTParty.get('http://api.npr.org/query?id=1056&fields=title,summary&output=JSON&apiKey=MDE5ODgxNTI1MDE0MzY5MTI3NDRkOWNhNQ001').to_s
+   @nprjson = JSON.parse(@npr)
 end
 end
