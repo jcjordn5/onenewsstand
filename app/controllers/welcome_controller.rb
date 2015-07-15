d526e1239d8e44b3db98e5f680b13fc546cd3ae0
Joseph Jordan <jcjordn5@gmail.com>
2015-07-15 10:27:59 -0400

    grasping apis and chrome_logger installed

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,6 @@
 class WelcomeController < ApplicationController
+  def index
+  @usatodaybreaking =  HTTParty.get('http://api.usatoday.com/open/breaking?expired=true&api_key=cq8p2k75c6qxqua5m58kd8df')
+  @guardian = HTTParty.get('http://content.guardianapis.com/search?q=politics&api-key=dc2tmawrmyg2xesbwr6q68d4')
+end
 end
