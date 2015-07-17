bef3a6f2a2486fe03a2eb125e727ba680ff72b39
Joseph Jordan <jcjordn5@gmail.com>
2015-07-17 14:04:54 -0400

    patch2

--- lib/soundcloud_api.rb
+++ lib/soundcloud_api.rb
@@ -1,16 +1,16 @@
-class SoundcloudApi
+#class SoundcloudApi
 
-def self.soundcloudwidget
-@sounds = []
-spull = HTTParty.get('https://api-v2.soundcloud.com/explore/pop?limit=2&offset=0&client_id=a94c27c1b7b5fbc10ddf13f53d57fbea')
-sclient = SoundCloud.new(:client_id => 'a94c27c1b7b5fbc10ddf13f53d57fbea')
-spull["tracks"].each do |x|
-track_url = x['permalink_url']
-embed_info = sclient.get('/oembed', :url => track_url)
-@sounds.push( embed_info['html'] )
-end
+#def self.soundcloudwidget
+#@sounds = []
+#spull = HTTParty.get('https://api-v2.soundcloud.com/explore/pop?limit=2&offset=0&client_id=a94c27c1b7b5fbc10ddf13f53d57fbea')
+#sclient = SoundCloud.new(:client_id => 'a94c27c1b7b5fbc10ddf13f53d57fbea')
+#spull["tracks"].each do |x|
+#track_url = x['permalink_url']
+#embed_info = sclient.get('/oembed', :url => track_url)
+#@sounds.push( embed_info['html'] )
+#end
 
-return @sounds
+#return @sounds
 
-end
-end
+#end
+#end
--- lib/twitter_api.rb
+++ lib/twitter_api.rb
@@ -1,18 +1,18 @@
-class TwitterApi
-
-def self.trending_tweets
-    client.trends(id=1, options = {})
-end
-
-def self.our_public_tweets
-    client.user_timeline("BBCNews", count: 1, exclude_replies: true, include_rts: false)
-end
-
-
-def self.client
-@client ||= Twitter::REST::Client.new do |config|
-  config.consumer_key        = "VfUgu5BvtP5rb9DJoRet03E7L"
-  config.consumer_secret     = "RL7dc2RvfjicxfWPvPnkpNszUxZ4wINLWEJ5ufsYSVhrdKTKGj"
-    end
-  end
-end
+# class TwitterApi
+#
+# def self.trending_tweets
+#     client.trends(id=1, options = {})
+# end
+#
+# def self.our_public_tweets
+#     client.user_timeline("BBCNews", count: 1, exclude_replies: true, include_rts: false)
+# end
+#
+#
+# def self.client
+# @client ||= Twitter::REST::Client.new do |config|
+#   config.consumer_key        = "VfUgu5BvtP5rb9DJoRet03E7L"
+#   config.consumer_secret     = "RL7dc2RvfjicxfWPvPnkpNszUxZ4wINLWEJ5ufsYSVhrdKTKGj"
+#     end
+#   end
+# end
