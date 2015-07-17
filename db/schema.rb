ea7973630b66cbe49bc198395375fa5a14266a53
Joseph Jordan <jcjordn5@gmail.com>
2015-07-17 09:05:00 -0400

    styling added

--- db/schema.rb
+++ db/schema.rb
@@ -24,11 +24,6 @@ ActiveRecord::Schema.define(version: 20150716180326) do
     t.datetime "updated_at",         null: false
   end
 
-  create_table "niles", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "nprs", force: :cascade do |t|
     t.string   "link"
     t.string   "title"
