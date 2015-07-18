938875412488a8d1ade6e60a64af83d92ade48b5
Niles Turner <turnerniles@gmail.com>
2015-07-18 14:54:58 -0400

    friday work

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
 
+
+
   def index
   @imgur = HTTParty.get('https://api.imgur.com/3/gallery.json', :header => {'Authorization' => 'Client-ID ' + '128a302fdff45ec'})
   #headers = { Authorization: "Client-ID 449d68ec45b2592"}
