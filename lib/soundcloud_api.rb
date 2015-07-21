72f32b13600b86040bf63b1c690708834dca3fc3
Niles Turner <turnerniles@gmail.com>
2015-07-21 17:03:39 -0400

    soundcloud edit

--- lib/soundcloud_api.rb
+++ lib/soundcloud_api.rb
@@ -2,7 +2,8 @@ class SoundcloudApi
 
 def self.soundcloudwidget
 @sounds = []
-spull = HTTParty.get('https://api-v2.soundcloud.com/explore/pop?limit=10&offset=0&client_id='+ENV['SoundCloudKey'])
+@categories = ['alternative+rock','ambient','classical','country','dance+&+edm','dancehall','deep+house','disco','drum+&+bass','dubstep','electronic','folk+&+singer-songwriter','hip+hop+&+rap','house','indie','jazz+&+blues','latin','metal','piano','pop','r&b+&+soul','reggae','reggaeton','rock','soundtrack','techno','trance','techno','trap','trip+hop','world']
+spull = HTTParty.get('https://api-v2.soundcloud.com/explore/'+@categories.sample+'?limit=10&offset=0&client_id='+ENV['SoundCloudKey'])
 sclient = SoundCloud.new(:client_id => ENV['SoundCloudKey'])
 spull["tracks"].each do |x|
 track_url = x['permalink_url']
